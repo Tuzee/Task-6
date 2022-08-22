424016997765df32ec93f8ab4631e7876ed5b2fc
Arthur <98693510+Tuzee@users.noreply.github.com>
2022-08-22 08:05:04 +0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,19 @@
+from cmath import sqrt
+import math
+
+def square_number():
+    result = sqrt(9)
+    return result
+
+def number_pi():
+    return 3.141592653589793
+    
+def poww():
+    result = int(math.pow(9, 3))
+    return result
+
+def hypott():
+    result = math.hypot(1, 2)
+    return result
+
+print(hypott())
